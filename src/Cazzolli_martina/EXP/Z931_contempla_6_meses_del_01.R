66fe380373bd0da8eb06e43768a01b276c274b1a
cazzollimartinacd <cazzollimartina@gmail.com>
2022-10-25 21:03:46 -0300

    Update Z931_contempla_6_meses_del_01.R
    Cambio de los meses

--- src/Cazzolli_martina/EXP/Z931_contempla_6_meses_del_01.R
+++ src/Cazzolli_martina/EXP/Z931_contempla_6_meses_del_01.R
@@ -18,13 +18,13 @@ PARAM$experimento <- "TS9310"
 
 PARAM$exp_input  <- "FE9250"
 
-PARAM$future       <- c( 202104 )
+PARAM$future       <- c( 202102 )
 
-PARAM$final_train  <- c(202012, 202101, 202011 )
+PARAM$final_train  <- c(202008, 202009, 202010, 202012, 202101, 202011)
 
-PARAM$train$training     <- c( 202008, 202009, 202010 )
-PARAM$train$validation   <- c( 202102 )
-PARAM$train$testing      <- c( 202103 )
+PARAM$train$training     <- c( 202008, 202009, 202010, 202011 )
+PARAM$train$validation   <- c( 202112 )
+PARAM$train$testing      <- c( 202101 )
 PARAM$train$undersampling  <- 1.0   # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
 PARAM$train$semilla  <- 975433
 # FIN Parametros del script
