fb538d103b69b690b84e3bc5b2cd897c2dc4902a
cazzollimartinacd <cazzollimartina@gmail.com>
2022-11-14 12:54:46 -0300

    Update z914_corregir_data_d.R

--- src/Cazzolli_martina/COMP_3/z914_corregir_data_d.R
+++ src/Cazzolli_martina/COMP_3/z914_corregir_data_d.R
@@ -14,9 +14,9 @@ require("data.table")
 
 #Parametros del script
 PARAM  <- list()
-PARAM$experimento  <- "DR9141"
+PARAM$experimento  <- "COMP_FINAL_DR9141"
 
-PARAM$exp_input  <- "CA9060"
+PARAM$exp_input  <- "COMP_FINAL_CA9060"
 
 #valores posibles  "ninguno" "rank_simple" , "rank_cero_fijo" , "deflacion"
 PARAM$metodo  <- "deflacion"
